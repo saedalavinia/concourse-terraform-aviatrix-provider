d0cbdfcb49c994ee3a227826d10984befbdb4eca
Saed Alavinia <saed.alavinia@gemalto.com>
2018-08-08 13:18:55 -0400

    Using HTTPS instead of GIT so that we don't have to pass trust host

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,8 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 go version
 mkdir -p $GOPATH/src/github.com/terraform-providers
 cd $GOPATH/src/github.com/terraform-providers
-git clone git@github.com:AviatrixSystems/terraform-provider-aviatrix
+git clone https://github.com/AviatrixSystems/terraform-provider-aviatrix.git
 cd $GOPATH/src/github.com/terraform-providers/terraform-provider-aviatrix
 make build
+
+mkdir -p $THIS_DIR/output/linux-amd64
